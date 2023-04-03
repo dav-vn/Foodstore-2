5fbb7a228ff9139529e44be20eff753be3495ac5
dav-vn <vartanyandg@gmail.com>
2023-04-04 02:32:02 +0300

    Fixing some errors

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,23 +1,25 @@
-import {Component, Input} from '@angular/core';
-import {Tag} from "../shared/modules/Tag";
-import {FoodService} from "../services/food/food.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+import { Tag} from "../shared/modules/Tag";
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.css']
 })
-export class TagsComponent {
-    @Input()
-    foodPageTags?: String[];
+export class TagsComponent implements OnInit {
 
-    @Input()
-    justifyContent: string = 'center';
-    tags?: Tag[];
-    constructor(private foodservice: FoodService) {
-    }
-    ngOnInit(): void {
-      if(!this.foodPageTags)
-      this.tags = this.foodservice.getAllTags()
-    }
+  @Input()
+  foodPageTags?:String[];
+
+  @Input()
+  justifyContent:string = 'center';
+
+  tags?:Tag[];
+  constructor(private foodService:FoodService) { }
+
+  ngOnInit(): void {
+    if(!this.foodPageTags)
+      this.tags = this.foodService.getAllTags();
+  }
 }
