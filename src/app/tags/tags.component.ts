80fa295e9fb8e635bee70593d9de6050e1ba63d3
dav-vn <vartanyandg@gmail.com>
2023-04-04 00:26:31 +0300

    Adding tag div into template file

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Tag} from "../shared/modules/Tag";
 
 @Component({
   selector: 'app-tags',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./tags.component.css']
 })
 export class TagsComponent {
-
+    tags: Tag[] = [];
 }
