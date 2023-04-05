47e7fcc9d250816b67f392e16141a1e0756e6e6b
dav-vn <vartanyandg@gmail.com>
2023-04-05 13:08:20 +0300

    Creating function addToCart

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Cart} from "../../shared/modules/Cart";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
-  constructor() { }
+  private cart:Cart = new Cart()
+  addToCart(food: Food): void {
+    let cartItem = this.cart.items.find(item => item.food.id === food.id);
+    if(cartItem) {
+      this.changeQuantity(food.id, cartItem.quantity + 1);
+      return;
+    }
+    this.cart.items.push(new CartItem(food);
+  }
 }
