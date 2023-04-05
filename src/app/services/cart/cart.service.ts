6eb6cf449431a2b96a87ae117fedf9beabc9318c
dav-vn <vartanyandg@gmail.com>
2023-04-05 13:15:37 +0300

    Creating changeQuantity function

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Cart} from "../../shared/modules/Cart";
+import { Food} from "../../shared/modules/Food";
+import {CartItem} from "../../shared/modules/CartItem";
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +14,16 @@ export class CartService {
       this.changeQuantity(food.id, cartItem.quantity + 1);
       return;
     }
-    this.cart.items.push(new CartItem(food);
+    this.cart.items.push(new CartItem(food));
   }
   removeFromCart(foodId: number): void {
     this.cart.items =
       this.cart.items.filter(item => item.food.id != foodId );
+  }
+
+  changeQuantity(foodId: number, quantity:number) {
+      let cartItem = this.cart.items.find(item => item.food.id === foodId)
+      if(!cartItem) return;
+      cartItem.quantity = quantity;
   }
 }
